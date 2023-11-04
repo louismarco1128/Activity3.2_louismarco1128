faead5069a7f59270905a70b389ad908dfb1f4d1
louismarco1128 <148313654+louismarco1128@users.noreply.github.com>
2023-11-04 11:23:46 +0800

    Update hero.sql

--- hero.sql
+++ hero.sql
@@ -5,10 +5,5 @@
     (9, 9, 109),  -- Leomord has Blade Armor
 
     (10, 10, 110);  -- Chou has Feather of Heaven
-
-
-
-
-    ALTER TABLE item_price
-
-    ADD Price DECIMAL (10,2);
+  ALTER TABLE item_price
+  ADD Price DECIMAL (10,2);
